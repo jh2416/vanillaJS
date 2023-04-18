4f40ee0b39309529f0c8a735a2b29d6104725bcb
jh2416 <wogns4576@gmail.com>
2023-04-18 23:50:25 +0900

    Update app.js
    variable

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 alert("hi");
 
 // console에서 작성
-2 + 2; //integer
+/*2 + 2; //integer
 1.2 + 6545; //float
 ("hello"); //string
-"hello" + "my name is hoon";
+"hello" + "my name is hoon";*/
+
+console.log(545454); //콘솔에 메시지 보내기(콘솔에서 출력)
+console.log("lalalalal");
+
+const a = 5; //상수;바뀌지 않는 값
+const b = 2;
+
+const myName = "hoon";
+const veryLongVariableName = 0; //camelcase
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
