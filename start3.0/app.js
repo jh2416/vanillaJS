94a9d291c77bcc2ccf376d14e82ce8395ec0a037
jh2416 <wogns4576@gmail.com>
2023-04-21 00:09:49 +0900

    #3.1 HTML in Javascript
    JS로 html element가져오기, 변경

--- start3.0/app.js
+++ start3.0/app.js
@@ -1,1 +1,6 @@
-document.title = "Hello! From JS!"; //JS로 HTML 변경
+//document.title = "Hello! From JS!"; //JS로 HTML 변경
+const title = document.getElementById("title"); //id로 가져오기, 그 이후 아무거나 변경 가능
+
+title.innerText = "Got you!";
+console.log(title.id);
+console.log(title.className);
