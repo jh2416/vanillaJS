5d91e79b483b86e873e6ced584696e8170e75030
jh2416 <wogns4576@gmail.com>
2023-04-21 01:22:52 +0900

    #3.2 Searching For Elements
    getElementsByClassName
    getElementsByTagName
    querySelector
    querySelectorAll

--- start3.0/app.js
+++ start3.0/app.js
@@ -1,6 +1,24 @@
 //document.title = "Hello! From JS!"; //JS로 HTML 변경
-const title = document.getElementById("title"); //id로 가져오기, 그 이후 아무거나 변경 가능
+/*const title = document.getElementById("title"); //id로 가져오기, 그 이후 해당항목 변경 가능
 
 title.innerText = "Got you!";
 console.log(title.id);
-console.log(title.className);
+console.log(title.className);*/
+
+/*const hellos = document.getElementsByClassName("hello"); //class로 가져오기, 여러개 가능
+
+console.log(hellos);*/
+
+const title = document.getElementsByTagName("h1"); //anchor, div, section, button...
+
+console.log(title);
+
+const cool = document.querySelector(".hello h1:first-child");
+// const cool = document.querySelector("div h1");
+//element를 CSS방식으로 검색가능
+//여러개인 경우 first element만 가져옴, 모두 가져오려면 querySelectorAll
+//const cool = document.querySelector("#hello"); == const cool = document.getElementById("hello");
+
+cool.innerText = "hello";
+
+console.log(cool);
