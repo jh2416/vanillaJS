64d06e0beaf1b27e3c6feb8c55c4f6578ab9c445
jh2416 <wogns4576@gmail.com>
2023-04-25 23:55:54 +0900

    #4.2 events

--- start4.0/app.js
+++ start4.0/app.js
@@ -2,7 +2,9 @@ const loginForm = document.getElementById("login-form"); //id를 찾고
 const loginInput = loginForm.querySelector("input"); //loginForm에서 바로 찾음
 const loginButton = loginForm.querySelector("button");
 //첫줄 지우고 2, 3줄 document.querySelector("#login-form input/botton");도 같은 코드
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(tomato);
   /*console.dir(loginInput.value); //input된 value
   console.log("click!!");*/
   const username = loginInput.value;
@@ -16,4 +18,6 @@ function onLoginBtnClick() {
   }*/
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+//loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+//브라우저는 엔터를 누를때 새로고침을 하고 form을 submit하도록 프로그래밍 되어있음-tomato추가 부분으로 확인
